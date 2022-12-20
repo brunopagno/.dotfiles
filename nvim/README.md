eb15e5ada04c7f40987a459c38fcc3d6e94c276c
Bruno Pagno <brunopagno@gmail.com>
2022-12-20 22:04:06 +0100

    now with formatting

--- nvim/README.md
+++
@@ -1,5 +0,0 @@
-## Things to do after copying these files
-- ~install packer~ (it should install itself, now)
-- it will be necessary to install language tools and LSPs manually, so check `./lua/gatoguild/lsp` for the ones that are currently setup
-- attempt to keep sane
-
--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -1,8 +1,15 @@
 local null_ls = require("null-ls")
 
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+local sources = {
+  formatting.prettier,
+  formatting.rubocop,
+  diagnostics.rubocop,
+}
+
 null_ls.setup({
-  null_ls.builtins.formatting.prettier,
-  null_ls.builtins.formatting.rubocop,
-  null_ls.builtins.diagnostics.eslint,
-  null_ls.builtins.diagnostics.rubocop,
+  sources = sources,
 })
+
