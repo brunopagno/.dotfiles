5410b0ab0092b5c4b9c3462623cb6198fed67b22
Bruno Pagno <brunopagno@proton.me>
2024-03-09 15:50:11 +0100

    dont use fancy tabs

--- dev/wezterm/wezterm.lua
+++ dev/wezterm/wezterm.lua
@@ -3,5 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'rose-pine-moon'
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 
 return config
