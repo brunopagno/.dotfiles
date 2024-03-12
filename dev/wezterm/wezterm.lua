b11e7fc1572a4ef7cc1c0b7d3edcc5a224df353a
Bruno Pagno <brunopagno@proton.me>
2024-03-12 22:12:49 +0100

    tabs on top

--- dev/wezterm/wezterm.lua
+++ dev/wezterm/wezterm.lua
@@ -3,7 +3,5 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'rose-pine'
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
 
 return config
