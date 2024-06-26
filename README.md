e2059d9bf33cef7b9ef659ef2fb84ed0408e3c61
Bruno Pagno <brunopagno@proton.me>
2024-06-26 22:01:31 +0200

    make things into script and magic

--- README.md
+++ README.md
@@ -4,33 +4,6 @@
 
 [Install arch gist file](https://gist.github.com/brunopagno/b395acb60f76df659480f0ef0dd5deb7).
 
-### Gnome things
-
-```bash
-gsettings set org.gnome.desktop.peripherals.keyboard delay 360
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25
-
-gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
-
-gsettings set org.gnome.mutter check-alive-timeout 20000
-gsettings set org.gnome.desktop.interface enable-hot-corners false
-```
-
-git defaults
-```bash
-git config --global init.defaultBranch main
-git config --global user.name "name"
-git config --global user.email "email"
-git config --global pager.branch false
-git config --global credential.helper store
-git config --global pull.rebase true
-```
-
 ### Things to be installed
 
 ```
--- setup.sh
+++ setup.sh
@@ -13,10 +13,52 @@ link_config() {
   fi
 }
 
-echo "Symlinking folders"
+echo "Symlink config folders"
 
-link_config "alacritty"
-link_config "nvim"
+read -e -p "[y/N]" choice
+if [[ "$choice" == [Yy]* ]] then
+  link_config "alacritty"
+  link_config "nvim"
+fi
 
-echo "setup complete"
+# --------------------
 
+echo "Gnome things..."
+ 
+read -e -p "[y/N]" choice
+if [[ "$choice" == [Yy]* ]] then
+  echo "set keyboard repeat speeds"
+  gsettings set org.gnome.desktop.peripherals.keyboard delay 360
+  gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 25
+  
+  echo "set workspaces and shortcuts"
+  gsettings set org.gnome.mutter dynamic-workspaces false
+  gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+  gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+  gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+  gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+  gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+  
+  echo "set other gnome settings"
+  gsettings set org.gnome.mutter check-alive-timeout 20000
+  gsettings set org.gnome.desktop.interface enable-hot-corners false
+fi
+
+# ------------------
+
+echo "Git things..."
+
+read -e -p "[y/N]" choice
+if [[ "$choice" == [Yy]* ]] then
+  echo "setting up git"
+  git config --global init.defaultBranch main
+  git config --global pager.branch false
+  git config --global credential.helper store
+  git config --global pull.rebase true
+fi
+
+echo "Done!"
