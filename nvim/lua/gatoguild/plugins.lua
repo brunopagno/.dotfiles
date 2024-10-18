519c4c2c5d730ce25311b44a2d28b823a2da3ca0
Bruno Pagno <brunopagno@proton.me>
2024-10-18 16:45:41 +0200

    add linting plugin (fix erb diagnostics)

--- nvim/lua/gatoguild/plugins.lua
+++ nvim/lua/gatoguild/plugins.lua
@@ -121,6 +121,21 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				eruby = { "erb_lint" },
+			}
+
+			vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
+	{
 		"stevearc/conform.nvim",
 		opts = {},
 		config = function()
