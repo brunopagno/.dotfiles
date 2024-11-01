a01de6811212024bf58a1513113ce0bb8d621348
Bruno Pagno <brunopagno@proton.me>
2024-11-01 20:51:26 +0100

    less workspaces

--- gato.sh
+++ gato.sh
@@ -49,15 +49,7 @@ case $command in
     
     echo "set workspaces and shortcuts"
     gsettings set org.gnome.mutter dynamic-workspaces false
-    gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
-    gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-    gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-    gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-    gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
-    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+    gsettings set org.gnome.desktop.wm.preferences num-workspaces 3
     
     echo "set other gnome settings"
     gsettings set org.gnome.mutter check-alive-timeout 20000
