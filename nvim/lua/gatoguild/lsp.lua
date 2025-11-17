fcc0594e8ebad2af04c5903d75bb92f5197badc4
Bruno Pagno <b.pagno@openproject.com>
2025-11-17 08:34:01 +0100

    go back to using nvim-lint because lsps are weird

--- nvim/lua/gatoguild/lsp.lua
+++ nvim/lua/gatoguild/lsp.lua
@@ -16,21 +16,12 @@ vim.lsp.config["luals"] = {
 }
 vim.lsp.enable("luals")
 
-vim.lsp.config["stylua"] = {
-	cmd = { "stylua", "--lsp" },
-	filetypes = { "lua" },
-	root_markers = { ".stylua.toml", "stylua.toml", ".editorconfig" },
-}
-vim.lsp.enable("stylua")
-
 -- TypeScript/JavaScript
 vim.lsp.config["tsserver"] = {
 	cmd = { "typescript-language-server", "--stdio" },
 	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
 	root_markers = { "package.json", ".git" },
 	on_attach = function(client, bufnr)
-		client.server_capabilities.documentFormattingProvider = false
-
 		vim.api.nvim_buf_create_user_command(bufnr, "OrganizeImports", function()
 			vim.lsp.buf.code_action({
 				context = {
@@ -43,33 +34,6 @@ vim.lsp.config["tsserver"] = {
 	end,
 }
 vim.lsp.enable("tsserver")
-
--- vscode-langservers-extracted
-vim.lsp.config["eslint"] = {
-	cmd = { "vscode-eslint-language-server", "--stdio" },
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-	workspace_required = true,
-	root_dir = function(bufnr, on_dir)
-		local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", ".git" }
-		on_dir(vim.fs.root(bufnr, root_markers))
-	end,
-	settings = {
-		validate = "on",
-		packageManager = nil,
-		experimental = {
-			useFlatConfig = false,
-		},
-		format = true,
-		quiet = false,
-		run = "onType",
-		problems = {
-			shortenToSingleLine = false,
-		},
-		nodePath = ".",
-		workingDirectory = { mode = "auto" },
-	},
-}
-vim.lsp.enable("eslint")
 
 -- Ruby
 vim.lsp.config["ruby-lsp"] = {
--- nvim/lua/gatoguild/plugins.lua
+++ nvim/lua/gatoguild/plugins.lua
@@ -45,39 +45,39 @@ require("lazy").setup({
 		},
 	},
 
-	-- LSP
-	-- {
-	-- 	"mfussenegger/nvim-lint",
-	-- 	config = function()
-	-- 		local lint = require("lint")
-	-- 		lint.linters_by_ft = {
-	-- 			eruby = { "erb_lint" },
-	-- 			typescript = { "eslint" },
-	-- 			javascript = { "eslint" },
-	-- 		}
+	-- langservers
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				eruby = { "erb_lint" },
+				typescript = { "eslint" },
+				javascript = { "eslint" },
+			}
 
-	-- 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-	-- 			callback = function()
-	-- 				lint.try_lint()
-	-- 			end,
-	-- 		})
-	-- 	end,
-	-- },
-	-- {
-	-- 	"stevearc/conform.nvim",
-	-- 	opts = {},
-	-- 	config = function()
-	-- 		require("conform").setup({
-	-- 			formatters_by_ft = {
-	-- 				lua = { "stylua" },
-	-- 				javascript = { "eslint_d" },
-	-- 				typescript = { "eslint_d" },
-	-- 				javascriptreact = { "eslint_d" },
-	-- 				typescriptreact = { "eslint_d" },
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "eslint_d" },
+					typescript = { "eslint_d" },
+					javascriptreact = { "eslint_d" },
+					typescriptreact = { "eslint_d" },
+				},
+			})
+		end,
+	},
 
 	-- autocompletions
 	{ "hrsh7th/cmp-nvim-lsp" },
